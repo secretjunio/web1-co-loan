29894d172d5210b8ad0799bf18d0c1f5ba3eac54
secretjunio <vuvanthien0812@gmail.com>
2019-11-15 19:02:50 +0700

    everything has update

---
+++ js/JavaScript1.js
@@ -0,0 +1,70 @@
+
+function tele1() {
+    document.getElementById("open1").style.display = "block";
+}
+function tele2() {
+    document.getElementById("open2").style.display = "block";
+}
+
+function tele3() {
+    document.getElementById("open3").style.display = "block";
+}
+
+function tele4() {
+    document.getElementById("open4").style.display = "block";
+}
+
+function tele5() {
+    document.getElementById("open5").style.display = "block";
+}
+
+function tele6() {
+    document.getElementById("open6").style.display = "block";
+}
+
+function tele7() {
+    document.getElementById("open7").style.display = "block";
+}
+
+
+function tele8() {
+    document.getElementById("open8").style.display = "block";
+}
+
+function tele9() {
+    document.getElementById("open9").style.display = "block";
+}
+
+function tele10() {
+    document.getElementById("open10").style.display = "block";
+}
+function close1() {
+    document.getElementById("open1").style.display = "none";
+}
+function close2() {
+    document.getElementById("open2").style.display = "none";
+}
+function close3() {
+    document.getElementById("open3").style.display = "none";
+}
+function close4() {
+    document.getElementById("open4").style.display = "none";
+}
+function close5() {
+    document.getElementById("open5").style.display = "none";
+}
+function close6() {
+    document.getElementById("open6").style.display = "none";
+}
+function close7() {
+    document.getElementById("open7").style.display = "none";
+}
+function close8() {
+    document.getElementById("open8").style.display = "none";
+}
+function close9() {
+    document.getElementById("open9").style.display = "none";
+}
+function close10() {
+    document.getElementById("open10").style.display = "none";
+}
---
+++ js/default.js
@@ -0,0 +1,8 @@
+﻿<script>
+    window.open('thongtin/thongtin 1.html', 'thongtin', 'height=255,width=250';
+
+    continued from previous line toolbar = no, directories = no, status = no, menubar = no;
+
+    continued from previous line scrollbars = no, resizable = 'no');
+
+</script>
